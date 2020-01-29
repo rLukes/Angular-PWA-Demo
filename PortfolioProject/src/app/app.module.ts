1c7e22690a31af7e5bba191b9e1a6ce23bd5be59
Ruben Lukes <lukesruben@gmail.com>
2020-01-29 14:33:45 +0100

    added fontawsome

--- PortfolioProject/src/app/app.module.ts
+++ PortfolioProject/src/app/app.module.ts
@@ -1,21 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { environment } from "../environments/environment";
+
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faGithub,
+  faCodepen,
+  faInstagram,
+  faYoutube,
+  faLinkedin,
+  faMedium
+} from "@fortawesome/free-brands-svg-icons";
+library.add(faGithub, faCodepen, faInstagram, faYoutube, faLinkedin, faMedium);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
+    FontAwesomeModule,
     BrowserModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register("ngsw-worker.js", {
+      enabled: environment.production
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
