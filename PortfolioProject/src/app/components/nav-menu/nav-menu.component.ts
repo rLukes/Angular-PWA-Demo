566cf94c6c85b086e2525ca15e275adc7e176c64
Ruben Lukes <lukesruben@gmail.com>
2020-01-29 20:53:53 +0100

    navigation menu

--- PortfolioProject/src/app/components/nav-menu/nav-menu.component.ts
+++ PortfolioProject/src/app/components/nav-menu/nav-menu.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-nav-menu',
-  templateUrl: './nav-menu.component.html',
-  styleUrls: ['./nav-menu.component.css']
+  selector: "app-nav-menu",
+  templateUrl: "./nav-menu.component.html",
+  styleUrls: ["./nav-menu.component.css"]
 })
 export class NavMenuComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  SidebarOn() {
+    var nav = document.getElementById("desktop-nav");
+    var toggle = document.getElementById("sidebar-toggle");
+    nav.classList.remove("display-none");
+    toggle.classList.add("display-none");
   }
 
+  SidebarOut() {
+    var nav = document.getElementById("desktop-nav");
+    var toggle = document.getElementById("sidebar-toggle");
+    nav.classList.add("display-none");
+    toggle.classList.remove("display-none");
+  }
 }
